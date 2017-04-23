89cb5c6bf4acdda7ae95213e5b28c7a2c4b257b5
Drew C <drewgamesaw@gmail.com>
2017-04-23 19:34:44 -0400

    add manifest file

---
+++ DOWNLOAD/LATEST/WIN/sha256.py
@@ -0,0 +1,9 @@
+import hashlib
+phases = [1, 2, 3, 4, 5]
+
+jsonString = '{\n'
+for phase in phases:
+    jsonString = jsonString + "phase_%d: " %phase + (hashlib.sha256(open("C:\lotaalpha\LOTA_GameData\phase_%d.zip" %phase, 'rb').read()).hexdigest()) + ",\n"
+    
+jsonString = jsonString[:-3] + '\n}'
+print(jsonString)
