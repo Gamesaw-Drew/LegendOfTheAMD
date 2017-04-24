df14b79b62ab3268220dfac6e9e2cadfcb03d097
Drew <drewgamesaw@gmail.com>
2017-04-23 22:15:55 -0400

    got it i think ma ybe

--- DOWNLOAD/LATEST/WIN/sha256.py
+++ DOWNLOAD/LATEST/WIN/sha256.py
@@ -1,9 +1,9 @@
 import hashlib
 phases = [1, 2, 3, 4, 5]
 
-jsonString = '{\n'
+jsonString = '{\n"data": ['
 for phase in phases:
-    jsonString = jsonString + '"phase_%d": ' %phase + '{"Filename": "phase_%d", "sha256" : "%s"},\n' %(phase, hashlib.sha256(open("phase_%d.zip" %phase, 'rb').read()).hexdigest())
+    jsonString = jsonString + '{"Filename": "phase_%d", "sha256" : "%s"},\n' %(phase, hashlib.sha256(open("phase_%d.zip" %phase, 'rb').read()).hexdigest())
     
-jsonString = jsonString[:-2] + '\n}'
+jsonString = jsonString[:-2] + '\n]}'
 print(jsonString)
