73440b1c65ea461f4251bfad152d6c265d9d894c
Drew <drewgamesaw@gmail.com>
2017-04-23 21:58:41 -0400

    launcher isbeing aids

--- DOWNLOAD/LATEST/WIN/sha256.py
+++ DOWNLOAD/LATEST/WIN/sha256.py
@@ -3,7 +3,7 @@ phases = [1, 2, 3, 4, 5]
 
 jsonString = '{\n'
 for phase in phases:
-    jsonString = jsonString + '"phase_%d": ' %phase + '"' + (hashlib.sha256(open("C:\lotaalpha\LOTA_GameData\phase_%d.zip" %phase, 'rb').read()).hexdigest()) + '",\n'
+    jsonString = jsonString + '"phase_%d": ' %phase + '"' + (hashlib.sha256(open("phase_%d.zip" %phase, 'rb').read()).hexdigest()) + '",\n'
     
 jsonString = jsonString[:-2] + '\n}'
 print(jsonString)
